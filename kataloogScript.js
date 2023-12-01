f9bdfbfa46517cc091d1384cb6372000ea89b644
Johannes <johannestammerand@gmail.com>
2023-12-01 17:54:59 +0200

    added script for catalogue with json

---
+++ kataloogScript.js
@@ -0,0 +1,32 @@
+import json from './tooted.json' assert {type: 'json'};
+
+function lisaToode(toode) {
+    var toodeteList = document.getElementsByClassName("tooted")[0];
+
+    var pilt = document.createElement("IMG");
+    pilt.src = "./images/" + toode.pildiNimi;
+    pilt.alt = toode.nimi;
+
+    var tekst1 = document.createElement("p");
+    var node1 = document.createTextNode(toode.nimi);
+    tekst1.appendChild(node1);
+    tekst1.className = "nimetus";
+
+    var tekst2 = document.createElement("p");
+    var node2 = document.createTextNode(toode.hind + '€');
+    tekst2.appendChild(node2);
+    tekst2.className = "hind";
+
+    var toodeObj = document.createElement("div");
+    toodeObj.className = "toode";
+    toodeObj.appendChild(pilt);
+    toodeObj.appendChild(tekst1);
+    toodeObj.appendChild(tekst2);
+
+    toodeteList.appendChild(toodeObj);
+}
+
+window.onload = function(){
+    var tooted = json.tooted;
+    tooted.forEach(lisaToode);
+}
